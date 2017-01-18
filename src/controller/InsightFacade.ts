a05d498d14c0a060e1f38de73ced0a265533f5ac
tiffanyxu819 <tiffanyxu819@gmail.com>
2017-01-18 09:02:21 -0800

    commit1

--- src/controller/InsightFacade.ts
+++ src/controller/InsightFacade.ts
@@ -4,6 +4,8 @@
 import {IInsightFacade, InsightResponse, QueryRequest} from "./IInsightFacade";
 
 import Log from "../Util";
+import {unzip} from "zlib";
+import {gzip} from "zlib";
 
 export default class InsightFacade implements IInsightFacade {
 
@@ -12,7 +14,74 @@ export default class InsightFacade implements IInsightFacade {
     }
 
     addDataset(id: string, content: string): Promise<InsightResponse> {
-        return null;
+
+        return new Promise(function (fulfill, reject) {
+                var rp = require('request-promise-native');
+                // TODO: implement
+
+            /*let LOP = [];
+
+            var zipFile = new JSZip();
+
+            var jsZipObject = zipFile.file(content);*/
+
+           /* for (let i of jsZipObject) {
+                LOP.push(rp(i));
+            }*/
+
+            var fs = require('fs');
+            var JSZip = require('jszip');
+
+            new JSZip.external.Promise(function (resolve, reject) {
+
+                fs.readFile(content, function (err, data) {
+
+                    if (err){
+                        reject(err);
+
+                    }else{
+                        resolve(data);
+                    }
+
+                });
+
+            }).then(function (data) {
+                return JSZip.loadAsync(data);
+
+            });
+
+
+          /*  fs.readFile(content, function (err:any, data:any) {
+                if (err) throw err;
+                JSZip.loadAsync(data).then(function (zip) {
+
+
+                });
+
+            })*/
+
+           /* zipFile.loadAsync(content)
+                .then*/
+
+/*
+            Promise.all(LOP)
+                .then( function(urls: string[]) {
+
+                    let jsonList = [];
+
+                    for (let url of urls) {
+                        try {
+                            let jsonObject = JSON.parse(url);
+                            //if (isEmpty(jsonObject)) reject('Error: No number was provided');
+                            jsonList.push(jsonObject);
+                        } catch (err) {
+                            reject('Error: Could not parse JSON');
+                        }}
+
+
+            fulfill();*/
+            });
+
     }
 
     removeDataset(id: string): Promise<InsightResponse> {
